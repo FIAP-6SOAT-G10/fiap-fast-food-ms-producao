e7f877956d2e7a6e82b7d32a1b164f06eeae2384
GabsMarcelino <gmarcelino@tractian.com>
2024-11-08 01:28:42 -0300

    feat: Context Wrapper and Viper Env Setup

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fiap-fast-food-ms-producao/adapter/context_manager"
+	"fiap-fast-food-ms-producao/infra/ctx"
 	"fiap-fast-food-ms-producao/main/router"
 	"fiap-fast-food-ms-producao/main/worker"
 	"fmt"
@@ -9,21 +11,23 @@ import (
 	"syscall"
 )
 
-func StartRouter() {
+func StartRouter(ctx context_manager.ContextManager) {
 	router := router.InitRouter()
-	router.Run(":8080")
+	port := ctx.Get("port")
+	router.Run(fmt.Sprintf(":%v", port))
 }
 
-func StartWorker() {
-	worker.InitWorker()
+func StartWorker(ctx context_manager.ContextManager) {
+	worker.InitWorker(ctx)
 }
 
 func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	ctx := ctx.NewContextManager()
 
-	go StartRouter()
-	go StartWorker()
+	go StartRouter(ctx)
+	go StartWorker(ctx)
 	<-quit
 	fmt.Println("Shutting down")
 }
--- main/worker/worker.go
+++ main/worker/worker.go
@@ -1,41 +1,60 @@
 package worker
 
 import (
+	"fiap-fast-food-ms-producao/adapter/context_manager"
 	"fmt"
 	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-func InitWorker() {
+func InitWorker(ctx context_manager.ContextManager) {
+
+	queueUrl := ctx.Get("aws_sqs_url")
+	aws_access_key_id := ctx.Get("aws_access_key_id")
+	aws_secret_access_key := ctx.Get("aws_secret_access_key")
+	aws_session_token := ctx.Get("aws_session_token")
+	aws_region := ctx.Get("aws_region")
+
 	sess := session.Must(session.NewSession(
 		&aws.Config{
-			Endpoint: aws.String(""),
-			Region:   aws.String("us-east-1"),
+			Endpoint:    aws.String(""),
+			Region:      aws.String(aws_region.(string)),
+			Credentials: credentials.NewStaticCredentials(aws_access_key_id.(string), aws_secret_access_key.(string), aws_session_token.(string)),
 		}))
-
 	sqsClient := sqs.New(sess)
 
 	fmt.Print("Started Worker")
 
 	for {
-		messages, err := receiveMessages(sqsClient)
+		messages, err := receiveMessages(ctx, sqsClient)
 		if err != nil {
 			log.Printf("Error receiving messages: %v", err)
 			continue
 		}
 		for _, msg := range messages {
 			log.Printf("Received message: %s", *msg.Body)
+
+			_, err := sqsClient.DeleteMessage(&sqs.DeleteMessageInput{
+				QueueUrl:      aws.String(queueUrl.(string)),
+				ReceiptHandle: msg.ReceiptHandle,
+			})
+			if err != nil {
+				log.Printf("Error deleting message: %v", err)
+				continue
+			}
+
 		}
 	}
 }
 
-func receiveMessages(sqsClient *sqs.SQS) ([]*sqs.Message, error) {
-	queueUrl := "https://sqs.us-east-1.amazonaws.com/730335514370/production-order-updates-queue"
+func receiveMessages(ctx context_manager.ContextManager, sqsClient *sqs.SQS) ([]*sqs.Message, error) {
+	queueUrl := ctx.Get("aws_sqs_url")
 	out, err := sqsClient.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(queueUrl),
+		QueueUrl:            aws.String(queueUrl.(string)),
 		MaxNumberOfMessages: aws.Int64(10),
 		WaitTimeSeconds:     aws.Int64(10),
 		VisibilityTimeout:   aws.Int64(10),
